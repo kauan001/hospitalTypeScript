9cbf13dda0bda9e97231043ebf21e3687f4b6c6e
kauan001 <kauanhenriquee123@hotmail.com>
2020-03-12 01:32:22 +0000

    terminado

--- hospital/paciente.ts
+++ hospital/paciente.ts
@@ -1,23 +1,17 @@
-  
-"use strict";
-var pessoa;
-(function (pessoa) {
-    var Pessoa = /** @class */ (function () {
-        function Pessoa() {
+namespace pessoa{
+    export class Paciente extends Pessoa{
+        private codPaciente: number;
+
+        getcodPaciente(){
+            
+            return this.codPaciente;
+
         }
-        Pessoa.prototype.getnome = function () {
-            return this.nome;
-        };
-        Pessoa.prototype.setnome = function (nome) {
-            this.nome = nome;
-        };
-        Pessoa.prototype.getcpf = function () {
-            return this.cpf;
-        };
-        Pessoa.prototype.setcpf = function (cpf) {
-            this.cpf = cpf;
-        };
-        return Pessoa;
-    }());
-    pessoa.Pessoa = Pessoa;
-})(pessoa || (pessoa = {}));
+
+        setcodPaciente(cod:number){
+
+            this.codPaciente = cod;
+            
+        }
+    }
+}
